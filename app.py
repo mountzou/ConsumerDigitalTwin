14432f41a816bbbe24445fb4469a2759511fef10
chrismountzouris <chrismountzou@gmail.com>
2022-08-07 22:24:12 +0300

    Minimal issue on breadcrumb

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import (Flask, render_template, redirect, url_for, request)
 from flask_mysqldb import MySQL
 import json
 import requests
+import datetime
 
 app = Flask(__name__)
 
@@ -37,15 +38,25 @@ def energy_production():
     solar_power_data = cur.fetchall()
     return render_template("energy-production.html", solar_power_data = json.dumps(solar_power_data))
 
-@app.route("/cdmp/")
+@app.route("/energy-consumption/")
 def cdmp():
     headers = {"X-API-TOKEN": '8a3cb21d-be27-466d-a797-54fae21a0d8a'}
     url = "https://twinergy.s5labs.eu/api/query/6158624e-be36-4a5f-9374-f04bb5b10e0d"
     response = requests.get(url, headers=headers)
     response1 = response.json()
     cdmp_data = json.dumps(response1, indent=4)
+    cdmp_data_json = json.loads(cdmp_data)
 
-    return render_template("cdmp.html", cdmp_data = cdmp_data)
+    air_condition_consumption = []
+    air_condition_time = []
+
+    for x in range(0,72,1):
+        d = datetime.datetime.strptime(cdmp_data_json[1][x]['DemandMeasurement']['observedDateTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        air_condition_consumption.append(cdmp_data_json[1][x]['DemandMeasurement']['totalConsumptionHourly'][0])
+        print(air_condition_consumption)
+        air_condition_time.append(datetime.date.strftime(d, "%d/%m/%y - %H:%M"))
+
+    return render_template("energy-consumption.html", air_condition_time = air_condition_time, air_condition_consumption = air_condition_consumption)
 
 if __name__ == "__main__":
     app.run(debug=True)
